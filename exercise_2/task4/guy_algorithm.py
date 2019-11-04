bf8686d57a3b78e77e30a41d04c380ec7e9c4b11
ahmad.qasim <ahmad.live.1@gmail.com>
2019-11-04 11:39:10 +0100

    Adding the comments of Felix from the implementation provided

--- exercise_2/task4/guy_algorithm.py
+++ exercise_2/task4/guy_algorithm.py
@@ -26,8 +26,13 @@ T = 5
 dt = T / NT
 time = np.linspace(0, T, NT)
 
+
+# this 'parameter' essentially defines how well the model approximates the true dynamics for this example.
+# Values below 0.05 yield essentially no visible difference to the truth, while values above 0.1
+# make the trajectories almost indistinguishable from noise, and only ensemble runs make it possible to evaluate
+# the error and estimate the true state.
 model_error = 1e-3
-# very smooth, entropy < 0
+
 # this is the error in the true model, and also in the observations. You do not need to change this.
 true_error = 1e-4
 m = 10  # ensemble runs
@@ -43,6 +48,9 @@ xt[0, :] = np.column_stack([x0 + np.random.randn(1) / 1000 for i in range(m * n)
 xm = xt.copy()
 for k in range(1, NT):
     for i in range(m):
+        # using this as "true dynamics" means there is no noise in the true states(which is also ok)
+        # xt[k,(i*n):((i+1)*n)]=(f_true(xt[k-1,(i*n):((i+1)*n)].reshape(1,-1), dt))
+        # test what happens if the "true dynamics" are just a less noisy version of the model
         xt[k, (i * nd):((i + 1) * nd)] = (f_model(xt[k - 1, (i * nd):((i + 1) * nd)].reshape(1, -1), dt, true_error))
         xm[k, (i * nd):((i + 1) * nd)] = (f_model(xm[k - 1, (i * nd):((i + 1) * nd)].reshape(1, -1), dt, model_error))
 
@@ -70,9 +78,9 @@ def normal_draw(cov):
 def observation(x):
     # relatively simple observation function z=h(x), also no change in dimension
     return -x / 2 + np.cos(x) / 3
-    # compute ensemble Kalman smoothing
 
 
+# compute ensemble Kalman smoothing
 def algorithm1_enks(z_data, error_covariance_m, error_covariance_q, observation, fhat_model):
     t = z_data.shape[0]
     m_l = error_covariance_m
@@ -113,6 +121,8 @@ def max_likelihood(xk, fhat_model):
             xhat = xk[k + 1, (i * nd):((i + 1) * nd)]
             data.append((xhat - fhat))
     data = np.row_stack(data)
+    # note that we do not compute it "per agent", as in the paper guy-2019b,
+    # but for all coordinates of the state (we only consider one "agent" in this code)
     return np.cov(data.T)
 
 
@@ -140,6 +150,10 @@ ax[0].plot(time[1:], zk[:, 0], 'g:', label='observation 1')
 ax[0].plot(time[1:], zk[:, 1], 'g.', label='observation 2')
 ax[0].set_title('true states and observations')
 ax[0].legend()
+
+# this second plot shows the estimated MODEL (!) state, but since the model here is just the truth plus noise,
+# the estimated model state should be the true state.
+# We also skip the first state in xm_hat, since it was chosen at random.
 ax[1].plot(time[2:], xm_hat[1:, 0], '-', label='estimated model state 1')
 ax[1].plot(time[2:], xm_hat[1:, 1], '-', label='estimated model state 2')
 ax[1].set_title('estimated states from observations and model')
