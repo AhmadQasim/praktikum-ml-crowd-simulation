54ac166bb34ae09c88d0fcf19a9d47ef1d27bb12
Martin <martin.meinel@tum.de>
2019-11-09 16:18:56 +0100

    Fix of bugs with velocity

--- exercise_2/task5/utils.py
+++ exercise_2/task5/utils.py
@@ -74,7 +74,7 @@ def parse_trajectory(path, delete_output=False):
 
     position_by_timestep['pedestrianId'] = position_by_timestep['pedestrianId'].apply(int)
 
-    pedestrian_data = np.empty((max(position_by_timestep.timeStep), max(position_by_timestep.pedestrianId), 4))
+    pedestrian_data = np.zeros((max(position_by_timestep.timeStep), max(position_by_timestep.pedestrianId), 4))
 
     for i, row in position_by_timestep.iterrows():
         pedestrian_id = row['pedestrianId']
@@ -88,17 +88,22 @@ def parse_trajectory(path, delete_output=False):
                                                            & (y <= pedestrian_velocities['endY-PID1'])]
 
         if velocity_row.empty:
-            print(f'No velocity row for x, y values of {(x, y)} and pedestrian ID of {pedestrian_id}')
+            # print(f'No velocity row for x, y values of {(x, y)} and pedestrian ID of {pedestrian_id}')
+            pedestrian_data[int(row.timeStep) - 1, int(row.pedestrianId) - 1, :2]  = [x, y]
             continue
         elif velocity_row.shape[0] > 1:
-            print(f'Found more than 1 velocity row for x, y values of {(x, y)} and pedestrian ID of {pedestrian_id}')
+            pass
+            # print(f'Found more than 1 velocity row for x, y values of {(x, y)} and pedestrian ID of {pedestrian_id}')
 
         duration = velocity_row['endTime-PID1'].iloc[0] - velocity_row['simTime'].iloc[0]
         vel_x = (velocity_row['endX-PID1'].iloc[0] - velocity_row['startX-PID1'].iloc[0]) / duration
         vel_y = (velocity_row['endY-PID1'].iloc[0] - velocity_row['startY-PID1'].iloc[0]) / duration
 
+        if vel_x == float('nan'):
+            vel_x = 0.0
+        if vel_y == float('nan'):
+            vel_y = 0.0
         pedestrian_data[int(row.timeStep) - 1, int(row.pedestrianId) - 1, :] = [x, y, vel_x, vel_y]
-
 
     if delete_output:
         shutil.rmtree(path)
@@ -110,35 +115,35 @@ def getModelPrediction(trueState, scenario_path: str, target_path: str, output_p
     vadere_root = '"/Users/mm/Desktop/Data Engineering and Analysis/3. Semester/Lab Course/vadere/"'
     edit_scenario(scenario_path, target_path, trueState)
 
-    os.system(f'java -jar {vadere_root}vadere-console.jar scenario-run ' # check vadere_root and play with the quotes
+    os.system(f'java -jar {vadere_root}vadere-console.jar scenario-run ' + # check vadere_root and play with the quotes
               f'--scenario-file {target_path} --output-dir="{output_path}"')
 
-def predictGNM(pedestrian_count):
+def predictGNM(pedestrian_count, timestep):
     path = './bottleneck/scenarios/bottleneck_gnm.json'
     targetPath = path[:-5] + '_edited.json'
     output_path = './bottleneck/output/GNM/prediction/'
     ped_data = parse_trajectory(path=f"./bottleneck/output/OSM/model/bottleneck_OSM_{pedestrian_count}")
-    getModelPrediction(ped_data[0], path, targetPath, output_path=output_path)
+    getModelPrediction(ped_data[timestep], path, targetPath, output_path=output_path)
     output_dir = os.listdir(output_path)[0]
     ped_predicted_data = parse_trajectory(os.path.join(output_path, output_dir), delete_output=True)
-    return ped_predicted_data
+    return ped_predicted_data[1]
 
-def predictOSM(pedestrian_count):
+def predictOSM(pedestrian_count, timestep):
     path = './bottleneck/scenarios/bottleneck_osm.json'
     targetPath = path[:-5] + '_edited.json'
     output_path = './bottleneck/output/OSM/prediction/'
     ped_data = parse_trajectory(path=f"./bottleneck/output/GNM/model/bottleneck_OSM_{pedestrian_count}")
-    getModelPrediction(ped_data[0], path, targetPath, output_path=output_path)
+    getModelPrediction(ped_data[timestep], path, targetPath, output_path=output_path)
     output_dir = os.listdir(output_path)[0]
     ped_predicted_data = parse_trajectory(os.path.join(output_path, output_dir), delete_output=True)
-    return ped_predicted_data
+    return ped_predicted_data[1]
 
 
 if __name__ == '__main__':
     # flag = 0 means OSM (ground truth) predict GNM, otherwise (GNM ground truth predict OSM) 1
     flag = 0
     if flag == 0:
-        ped_predicted_data = predictGNM()
+        ped_predicted_data = predictGNM(15)
     else:
-        ped_predicted_data = predictOSM()
-    print(ped_predicted_data[0])
+        ped_predicted_data = predictOSM(15)
+    print(ped_predicted_data.shape)
