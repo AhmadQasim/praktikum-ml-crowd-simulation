619eaadeacde6618562d4851be1a520bc04c2611
Martin <martin.meinel@tum.de>
2019-11-28 10:11:58 +0100

    Code Adjustments for Task 5 first part

--- exercise_3/task5/task5-1.py
+++ exercise_3/task5/task5-1.py
@@ -2,11 +2,12 @@ from utils import edit_scenario, plot_phase_portrait_first_part, create_plot, ru
 import os
 
 if __name__ == '__main__':
-    plot_phase_portrait_first_part(163-68, filter(lambda f: not f.startswith('.'), os.listdir('./outputs/')),
-                                   pedestrian_id=3)
-    create_plot(3)
-    y = 4.5
-    edit_scenario("Bottleneck bifurcation.scenario", y=y)
-    run_simulation("'/Users/mm/Desktop/Data Engineering and Analysis/3. Semester/Lab Course/vadere/'",
-                   "../scenarios/'Bottleneck bifurcation.scenario'",
-                   f'../outputs/{y}/')
+    for i in range(1, 20, 1):
+        plot_phase_portrait_first_part(163-68, filter(lambda f: not f.startswith('.') and not f.startswith('s'), os.listdir('../outputs/')),
+                                   pedestrian_id=i)
+    #create_plot(3)
+    #y = 4.5
+    #edit_scenario("Bottleneck bifurcation.scenario", y=y)
+    #run_simulation("'/Users/mm/Desktop/Data Engineering and Analysis/3. Semester/Lab Course/vadere/'",
+     #              "../scenarios/'Bottleneck bifurcation.scenario'",
+      #             f'../outputs/{y}/')
