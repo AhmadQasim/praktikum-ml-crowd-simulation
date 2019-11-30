bbe44815751f0cf5844216e0dfe2454c392c7059
GIZA\Kaan <kaan.atukalp@tum.de>
2019-11-30 12:39:27 +0100

    fix the centering problem of pca

--- exercise_4/pca.py
+++ exercise_4/pca.py
@@ -3,9 +3,10 @@ import numpy as np
 class PCA:
     def __init__(self):
         self.S: [None, np.ndarray] = None
-        self.V_T: [None, np.ndarray] = None
+        self.V: [None, np.ndarray] = None
         self.is_fitted: bool = False
         self.max_dimensions: int = -1
+        self.mean = None
 
     def fit(self, x: np.ndarray) -> 'PCA':
         if len(x.shape) != 2:
@@ -14,13 +15,14 @@ class PCA:
         x = x.copy()
         N, self.max_dimensions = x.shape
 
-        x -= x.mean(axis=0).reshape(1, -1)
+        self.mean = x.mean(axis=0).reshape(1, -1)
+        x -= self.mean
 
         # set V
-        eigenvalues_V, self.V_T = np.linalg.eig(x.T @ x)
+        eigenvalues_V, self.V = np.linalg.eig(x.T @ x)
         indices = eigenvalues_V.argsort()[::-1]
         eigenvalues_V = eigenvalues_V[indices]
-        self.V_T = self.V_T[:, indices]
+        self.V = self.V[:, indices]
 
         # set S, only save the diagonal
         if self.max_dimensions > N:
@@ -35,12 +37,12 @@ class PCA:
             raise RuntimeError
         if len(x.shape) != 2 or x.shape[1] != self.max_dimensions:
             raise ValueError
-        return x @ self.V_T
+        return (x - self.mean) @ self.V
 
     def inverse_transform(self, US: np.ndarray) -> np.ndarray:
-        if US.shape[1] > self.V_T.shape[0]:
+        if US.shape[1] > self.V.shape[0]:
             raise ValueError
-        return US @ self.V_T.T[:US.shape[1], :]
+        return US @ self.V.T[:US.shape[1], :] + self.mean
 
     def fit_transform(self, x: np.ndarray) -> np.ndarray:
         return self.fit(x).transform(x)
