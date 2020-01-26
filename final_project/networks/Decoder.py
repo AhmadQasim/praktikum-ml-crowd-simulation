c41b9d996fc96aeaf9780c648d37152eca94aea0
ahmad.qasim <ahmad.live.1@gmail.com>
2020-01-26 16:36:05 +0100

    adding the decoder and poolingmodule classes

---
+++ final_project/networks/Decoder.py
@@ -0,0 +1,78 @@
+import torch
+import torch.nn as nn
+import os
+os.chdir("../..")
+from sgan.networks.PoolingModule import PoolingModule
+
+
+class Decoder(nn.Module):
+    def __init__(
+        self,
+        seq_len,
+        embedding_dim=64,
+        hidden_dim=128,
+        ffnn_dim=1024,
+        num_layers=1,
+        dropout=0.0,
+        bottleneck_dim=1024
+    ):
+        super(Decoder, self).__init__()
+
+        self.seq_len = seq_len
+        self.ffnn_dim = ffnn_dim
+        self.hidden_dim = hidden_dim
+        self.embedding_dim = embedding_dim
+
+        self.decoder = nn.LSTM(embedding_dim, hidden_dim, num_layers, dropout=dropout)
+
+        self.pool_net = PoolingModule(
+            embedding_dim=self.embedding_dim,
+            hidden_dim=self.hidden_dim,
+            ffnn_dim=ffnn_dim,
+            bottleneck_dim=bottleneck_dim
+        )
+
+        self.fc_0 = nn.Linear(hidden_dim + bottleneck_dim, ffnn_dim)
+        self.bn_0 = nn.BatchNorm1d(ffnn_dim)
+        self.relu_0 = nn.ReLU()
+        self.fc_1 = nn.Linear(ffnn_dim, hidden_dim)
+        self.bn_1 = nn.BatchNorm1d(hidden_dim)
+        self.relu_1 = nn.ReLU()
+
+        self.embedding_0 = nn.Linear(2, embedding_dim)
+        self.fc_2 = nn.Linear(hidden_dim, 2)
+
+    def forward(self, last_position, last_pos_rel, state_tuple, seq_start_end):
+        batch = last_position.size(0)
+        pred_traj_fake_relative = []
+        decoder_input = self.embedding_0(last_pos_rel)
+        decoder_input = decoder_input.view(1, batch, self.embedding_dim)
+
+        for _ in range(self.seq_len):
+            output, state_tuple = self.decoder(decoder_input, state_tuple)
+            relative_position = self.fc_2(output.view(-1, self.hidden_dim))
+            current_position = relative_position + last_position
+
+            hidden_decoder = state_tuple[0]
+            pool_h = self.pool_net(hidden_decoder, seq_start_end, current_position)
+            hidden_decoder = torch.cat([hidden_decoder.view(-1, self.hidden_dim), pool_h], dim=1)
+
+            hidden_decoder = self.fc_0(hidden_decoder)
+            hidden_decoder = self.bn_0(hidden_decoder)
+            hidden_decoder = self.relu_0(hidden_decoder)
+            hidden_decoder = self.fc_1(hidden_decoder)
+            hidden_decoder = self.bn_1(hidden_decoder)
+            hidden_decoder = self.relu_1(hidden_decoder)
+
+            hidden_decoder = torch.unsqueeze(hidden_decoder, 0)
+            state_tuple = (hidden_decoder, state_tuple[1])
+
+            embedding_input = relative_position
+
+            decoder_input = self.embedding_0(embedding_input)
+            decoder_input = decoder_input.view(1, batch, self.embedding_dim)
+            pred_traj_fake_relative.append(relative_position.view(batch, -1))
+            last_position = current_position
+
+        pred_traj_fake_relative = torch.stack(pred_traj_fake_relative, dim=0)
+        return pred_traj_fake_relative, state_tuple[0]
---
+++ final_project/networks/PoolingModule.py
@@ -0,0 +1,56 @@
+import torch
+import torch.nn as nn
+import os
+os.chdir("../..")
+
+
+class PoolingModule(nn.Module):
+    def __init__(
+        self,
+        embedding_dim=64,
+        hidden_dim=64
+    ):
+        super(PoolingModule, self).__init__()
+
+        self.hidden_dim = hidden_dim
+        self.bottleneck_dim = 1024
+        self.embedding_dim = embedding_dim
+
+        self.fc_0 = nn.Linear(embedding_dim + hidden_dim, 512)
+        self.bn_0 = nn.BatchNorm1d(512)
+        self.relu_0 = nn.ReLU()
+        self.fc_1 = nn.Linear(512, 1024)
+        self.bn_1 = nn.BatchNorm1d(1024)
+        self.relu_1 = nn.ReLU()
+
+        self.embedding_0 = nn.Linear(2, embedding_dim)
+
+    def forward(self, h_states, seq_start_end, end_pos):
+        hidden_pool_all = []
+        for (start, end) in seq_start_end:
+            start = start.item()
+            end = end.item()
+
+            num_ped = end - start
+            hidden_state = h_states.view(-1, self.hidden_dim)[start:end]
+            end_positions = end_pos[start:end]
+
+            hidden_interleaved = hidden_state.repeat(num_ped, 1)
+            end_interleaved = end_positions.repeat(num_ped, 1)
+            end_repeated = end_positions.unsqueeze(dim=1).repeat(1, num_ped, 1)
+            end_repeated = end_repeated.view(-1, end_positions.size(1))
+            relative_positions = end_interleaved - end_repeated
+            relative_embeddings = self.embedding_0(relative_positions)
+            ffnn_h_input = torch.cat([relative_embeddings, hidden_interleaved], dim=1)
+
+            hidden_pool = self.fc_0(ffnn_h_input)
+            hidden_pool = self.bn_0(hidden_pool)
+            hidden_pool = self.relu_0(hidden_pool)
+            hidden_pool = self.fc_1(hidden_pool)
+            hidden_pool = self.bn_1(hidden_pool)
+            hidden_pool = self.relu_1(hidden_pool)
+
+            hidden_pool = hidden_pool.view(num_ped, num_ped, -1).max(1)[0]
+            hidden_pool_all.append(hidden_pool)
+        hidden_pool_all = torch.cat(hidden_pool_all, dim=0)
+        return hidden_pool_all
