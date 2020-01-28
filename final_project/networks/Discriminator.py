d013e33689e4637c61de0e6128d8e92861438d63
GIZA\Kaan <kaan.atukalp@tum.de>
2020-01-28 20:49:22 +0100

    add documentation to discriminator

--- final_project/networks/Discriminator.py
+++ final_project/networks/Discriminator.py
@@ -10,9 +10,9 @@ class Discriminator(nn.Module):
     def __init__(self, in_dim, latent_dim, activation_encoder=False):
         """
 
-        :param in_dim:
-        :param latent_dim:
-        :param activation_encoder:
+        :param in_dim: input dimension
+        :param latent_dim: dimension of the embedding
+        :param activation_encoder: whether the encoder should have activation or not
         """
         super().__init__()
 
@@ -31,10 +31,10 @@ class Discriminator(nn.Module):
     def forward(self, trajectory, relative_trajectory, seq_start_end):
         """
 
-        :param trajectory:
-        :param relative_trajectory:
-        :param seq_start_end:
-        :return:
+        :param trajectory: trajectory data
+        :param relative_trajectory: relative trajectory data, i.e. changes of coordinates
+        :param seq_start_end: start and end indices for each sequence in trajectory data
+        :return: return the classification scores of trajectories
         """
         encoded_relative_trajectory = self.encoder(relative_trajectory)
 
